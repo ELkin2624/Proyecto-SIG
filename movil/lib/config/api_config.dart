d82f43651e8ce1bc13860644c74a8b49b7c5ddc5
Elkin Apaza <apazaedixon98@gmail.com>
2025-12-08 18:48:10 -0300

    ip cambiada

--- movil/lib/config/api_config.dart
+++ movil/lib/config/api_config.dart
@@ -1,4 +1,4 @@
 // movil/lib/config/api_config.dart
 class ApiConfig {
-   static const String baseUrl = 'http://192.168.0.32:8000';
+   static const String baseUrl = 'http://192.168.16.94:8000';
 }
